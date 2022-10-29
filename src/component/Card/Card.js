bcf7259932ef09bd28d4dcddcb8fdaa1b52164ab
berkay oren <benberkayoren@gmail.com>
2022-10-29 13:39:58 +0300

    tobefixed

--- src/component/Card/Card.js
+++ src/component/Card/Card.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./Card.css";
+import defaultImage from "../../assets/default-image.jpg";
 
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div className="cardContainer">
+      <h4>{recipe.label}</h4>
+
+      <img src={defaultImage} alt="" className="recipeImage" />
+      <button>View More</button>
+    </div>
+  );
 };
 
 export default Card;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Home.css";
 import Search from "../../component/Search/Search";
 import Card from "../../component/Card/Card";
+import homeSvg from "../../assests/home.svg";
 import { useState } from "react";
 const Home = () => {
   const [recipe, setRecipe] = useState();
@@ -9,9 +10,15 @@ const Home = () => {
   return (
     <div>
       <Search setRecipe={setRecipe} />
-      <div className="resultContainer">
-        <Card />
-      </div>
+      {recipe ? (
+        <div className="resultContainer"> {recipe.map((item,i)=>( <Card key={i} recipe={item.recipe}/> }))} 
+          <Card />
+        </div>
+      ) : (
+        <div className="homeImg">
+          <img src={homeSvg} alt="" />
+        </div>
+      )}
     </div>
   );
 };
