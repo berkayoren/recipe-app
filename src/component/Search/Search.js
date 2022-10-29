42c7b4b6b416af5bfade62c41ac7740e7c884191
berkay oren <benberkayoren@gmail.com>
2022-10-29 13:20:44 +0300

    search

--- src/component/Search/Search.js
+++ src/component/Search/Search.js
@@ -1,19 +1,48 @@
 import React from "react";
+import { useState } from "react";
 import "./Search.css";
-const mealTypes = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
+import axios from "axios";
 
-const Search = () => {
+const mealTypes = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
+const APP_ID = "3161a284";
+const APP_KEY = "a5ffabaa8e1de132cadafc34ed2f7b6b";
+
+const Search = ({ setRecipe }) => {
+  const [meal, setMeal] = useState(mealTypes[0].toLowerCase());
+  const [value, setValue] = useState("");
+  const url = `https://api.edamam.com/search?q=${value}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${meal}`;
+  const getRecipe = async (e) => {
+    e.preventDefault();
+    if (value === "") return;
+    else {
+      const result = await axios.get(url);
+
+      setRecipe(result.data.hits);
+    }
+    setValue("");
+  };
+
   return (
     <div className="search">
       <h2>Food App</h2>
       <div className="formContainer">
-        <form>
-          <input className="food" type="text" />
+        <form onSubmit={getRecipe}>
+          <input
+            className="food"
+            type="text"
+            onChange={(e) => setValue(e.target.value)}
+            value={value}
+          />
           <button>Search</button>
         </form>
-        <select className="foodSelect" name="mealTypes" id="mealTypes">
+        <select
+          className="foodSelect"
+          name="mealTypes"
+          id="mealTypes"
+          onChange={(e) => setMeal(e.target.value)}
+        >
           {mealTypes.map((item, i) => (
-            <option key={i} value={item}>
+            <option key={i} value={item.toLowerCase()}>
               {item}
             </option>
           ))}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,10 +2,13 @@ import React from "react";
 import "./Home.css";
 import Search from "../../component/Search/Search";
 import Card from "../../component/Card/Card";
+import { useState } from "react";
 const Home = () => {
+  const [recipe, setRecipe] = useState();
+  console.log(recipe);
   return (
     <div>
-      <Search />
+      <Search setRecipe={setRecipe} />
       <div className="resultContainer">
         <Card />
       </div>
